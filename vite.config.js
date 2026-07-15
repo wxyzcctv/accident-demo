e01c154e9bc03b3223f83b8bc9d9802aba838cdb
wxyzcctv <2846629226@qq.com>
2026-07-15 20:55:12 +0800

    更新配置

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  // 关键配置：github仓库名，前后斜杠不能丢
+  base: '/accident-demo/',
+  build: {
+    outDir: 'dist'
+  },
   resolve: {
     alias: {
       '@': '/src'
